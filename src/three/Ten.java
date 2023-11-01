68403654f1e55804c36b930590843a237deec6eb
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-11-01 11:08:49 +0900

    BAEKJOON 3-10

---
+++ src/three/Ten.java
@@ -0,0 +1,20 @@
+package three;
+
+import java.util.Scanner;
+
+public class Ten {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        num = sc.nextInt();
+        for (int x = 1; x <= num; x++){
+            for(int y = 1; y <= num - x ; y++){
+                System.out.print(" ");
+            }
+            for (int k = 0; k < x; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
