69ce19b47f48fdb99c3c43823758e4ce7baaa719
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-11-02 11:23:54 +0900

    BAEKJOON 3-11 #10952

--- src/three/Twelve.java
+++ src/three/Twelve.java
@@ -1,7 +1,15 @@
 package three;
 
+import java.util.Scanner;
+
 public class Twelve {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        while(sc.hasNextInt()) {		// EOF처리를 위해 Scanner의 메소드의 hasNext()로 처리
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
     }
 }
